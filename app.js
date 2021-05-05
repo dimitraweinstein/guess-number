0eb361c331b65c5cbaca25db21aca136c388360e
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-05 11:32:45 -0700

    refactored functions and corrected results and feedback to reduce guesses remaining

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
 // import functions and grab DOM elements
-    import { compareNumbers } from './utils.js';
+import { compareNumbers } from './utils.js';
 
 const userInput = document.getElementById('user-input');
 const submitButton = document.getElementById('submit-button');
 const resetButton = document.getElementById('reset-button');
 const guessRemainDiv = document.getElementById('guess-remain-div');
+const resultsDiv = document.getElementById('results-div');
 
 // initialize state
 const randomNumber = Math.ceil(Math.random() * 20);
-let guessRemain = '3';
-
-
-
+let guessRemain = 3;
 
 
 // set event listeners to update state and DOM
 submitButton.addEventListener('click', () => {
+    guessRemain--;
     
     const inputValue = Number(userInput.value);
 
     console.log('this button works');
 
-    compareNumbers(inputValue, randomNumber);
+    const results = compareNumbers(inputValue, randomNumber);
 
-    // resultsDiv.textContent = ;
-
-    if (userInput > randomNumber || userInput < randomNumber) {
-        return guessRemain--;
+    if (results === 0) {
+        return guessRemainDiv.textContent = `Correct! You Guessed The Right Number! Try your luck and guess again!`;
     }
-
-    else {
-        return;
+    
+    else if (results === 1) {
+        return guessRemainDiv.textContent = `Sorry! your guess is higher than the chosen number. Guess again. You have ${guessRemain} number guesses remaining.`;
     }
+    
+    else if (results === -1) {
+        return guessRemainDiv.textContent = `Sorry! your guess is lower than the chosen number. Guess again. You have ${guessRemain} number guesses remaining.`;
+    }
+    
+
 
 });
 
 
 
-resetButton.addEventListener ('click', () => {
+// resetButton.addEventListener ('click', () => {
     
     
 
-});
+// });
--- utils.js
+++ utils.js
@@ -1,21 +1,21 @@
 export function compareNumbers(userInput, randomNumber) {
-    const resultsDiv = document.getElementById('results-div');
+    
+ 
 
     if (userInput === randomNumber) {
-        return resultsDiv.textContent = `Correct! You Guessed The Right Number! You have ${3} number guesses remaining.`;
+        return 0;
     }
 
     if (userInput > randomNumber) {
-        return resultsDiv.textContent = `Sorry! your guess is higher than the chosen number. Guess again. You have ${3} number guesses remaining.`;
+        return 1;
     }
 
     if (userInput < randomNumber) {
-        return resultsDiv.textContent = `Sorry, your guess is lower than the chosen number. Guess again. You have ${3} number guesses remaining.`;
+        return -1;
     }
 
     else {
-        return resultsDiv.textContent =  `Try your luck at guessing the secret chosen number! You have ${3} number guesses remaining.`;
+        return;
     }
 
-
-};
+}
